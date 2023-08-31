95ae03bc3a750d7f3ed3b02e3abf30ea33d31a86
Peter M. Elias <petermelias@gmail.com>
2023-08-31 01:01:51 -0500

    add some more charts to engagement dashboard and swappable pkgs

--- engagement-dashboard-snowflake/src/app/page.tsx
+++ engagement-dashboard-snowflake/src/app/page.tsx
@@ -1,35 +1,155 @@
-import { Card, Title, LineChart, BarChart } from "@tremor/react";
-import { FactsAppEngagement } from "app-engagement";
+import {
+  Col,
+  Card,
+  Grid,
+  Title,
+  Text,
+  BarChart,
+  DonutChart,
+  Tab,
+  TabList,
+  TabGroup,
+  TabPanel,
+  TabPanels,
+  Table,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@tremor/react";
+
+import { FactsAppEngagement as DataPkg } from "app-engagement";
+import { FactsAppEngagement as DataPkgFast } from "snowflake-demo-package-fast";
+
+const queries = (pkg: any) => ({
+  genderSplit: pkg.select(
+    pkg.fields.gender,
+    pkg.fields.gender.count().as('genderCount'))
+    .filter(pkg.fields.gender.isNotNull()),
+  ageSplit: pkg.select(
+    pkg.fields.age,
+    pkg.fields.age.count().as('ageCount'))
+    .filter(pkg.fields.age.isNotNull()),
+  ethnicitySplit: pkg.select(
+    pkg.fields.ethnicity,
+    pkg.fields.ethnicity.count().as('ethnicityCount')),
+  topSessions: pkg.select(
+    pkg.fields.devicemakemodel,
+    pkg.fields.appTitle,
+    pkg.fields.foregroundduration)
+   .orderBy([pkg.fields.foregroundduration, 'DESC'])
+});
 
 export default async function Home() {
 
-  const {
-    age,
-    appTitle
-  } = FactsAppEngagement.fields;
+  const activePkg = DataPkgFast;
 
-  const chartData = await FactsAppEngagement.select(
-    age,
-    appTitle.countDistinct().as("appCount"))
-  .orderBy([age, 'ASC'])
-  .filter(age.isNotNull())
-  .limit(100000)
-  .execute();
+  const [
+    genderData,
+    ageData,
+    ethnicityData,
+    sessionData
+  ] = await (Promise.all([
+    queries(activePkg)
+    .genderSplit
+    .execute(),
+    queries(activePkg)
+    .ageSplit
+    .execute(),
+    queries(activePkg)
+    .ethnicitySplit
+    .execute(),
+    queries(activePkg)
+    .topSessions
+    .limit(50)
+    .execute()
+  ]));
 
-  const fmtData = chartData.map((o: any) => ({ age: o.AGE.toString(), appCount: o.appCount.toString() }));
+  console.log(sessionData);
 
   return (
-    <Card>
-      <Title>App Count By Age</Title>
-      <BarChart
-        className="mt-8 h-800px"
-        data={fmtData}
-        index="age"
-        categories={["appCount"]}
-        colors={["red"]}
-        yAxisWidth={40}
-        showLegend={true}
-      />
-    </Card>
+    <main className="p-12">
+      <Title>Data Packages on Snowflake</Title>
+      <Text>Uses Data Packages to embed Snowflake data in a React App. Tremor charts for visualization.</Text>
+
+      <TabGroup className="mt-6">
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Drilldowns</Tab>
+        </TabList>
+
+        <TabPanels>
+
+          <Grid numItems={3} className="mt-6 gap-6">
+            <Card>
+              <Title>Gender Breakdown</Title>
+              <DonutChart
+                className="mt-6"
+                data={genderData}
+                category="genderCount"
+                index="GENDER"
+                colors={["rose", "violet"]}
+              />
+            </Card>
+
+            <Card>
+              <Title>Age Breakdown</Title>
+              <DonutChart
+                className="mt-6"
+                data={ageData}
+                category="ageCount"
+                index="AGE"
+                colors={["rose", "violet", "slate", "indigo", "cyan", "amber"]}
+              />
+            </Card>
+
+            <Card>
+              <Title>Ethnicity Breakdown</Title>
+              <DonutChart
+                className="mt-6"
+                data={ethnicityData}
+                category="ethnicityCount"
+                index="ETHNICITY"
+                colors={["rose", "violet", "slate", "indigo", "cyan", "amber"]}
+              />
+            </Card>
+
+            <Col numColSpan={3}>
+              <Card>
+                <Title>Top Sessions By Duration</Title>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableHeaderCell>Duration</TableHeaderCell>
+                      <TableHeaderCell>App</TableHeaderCell>
+                      <TableHeaderCell>Device</TableHeaderCell>
+                    </TableRow>
+                  </TableHead>
+
+                  <TableBody>
+                    {sessionData.map((row: any) => (
+                      <TableRow key={row.foregroundduration}>
+                        <TableCell>{row.foregroundduration}</TableCell>
+                        <TableCell>
+                          <Text>{row.appTitle}</Text>
+                        </TableCell>
+                        <TableCell>
+                          <Text>{row.devicemakemodel}</Text>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+
+                </Table>
+              </Card>
+            </Col>
+          </Grid>
+
+
+
+        </TabPanels>
+      </TabGroup>
+    </main>
   )
 };
