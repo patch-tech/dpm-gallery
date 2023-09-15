b6b96278bc68f90f262e54dd53660d90d1841368
Peter M. Elias <petermelias@gmail.com>
2023-09-15 14:33:33 -0500

    override webpack config

--- engagement-dashboard-snowflake/next.config.js
+++ engagement-dashboard-snowflake/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        config.resolve.symlinks = false;
+        return config;
+    }
+}
 
 module.exports = nextConfig
