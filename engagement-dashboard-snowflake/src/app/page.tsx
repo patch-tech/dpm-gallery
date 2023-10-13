043d59452c80a344b72b39780e3c4bb1802b9595
Whelan Boyd <whelanboyd@whelans-air.myfiosgateway.com>
2023-10-13 17:12:02 -0400

    add time series and change donuts

--- engagement-dashboard-snowflake/src/app/page.tsx
+++ engagement-dashboard-snowflake/src/app/page.tsx
@@ -1,63 +1,79 @@
 import {
-  Col, Card,Grid, Title, Text, BarChart, DonutChart, Tab, TabList,
-  TabGroup, TabPanel, TabPanels, Table, TableHead, TableHeaderCell, TableRow, TableCell,TableBody,
+  Col, Card,Grid, Title, Text, DonutChart, Tab, TabList, BarChart, LineChart,
+  TabGroup, TabPanel, TabPanels, Table, TableHead, TableHeaderCell, TableRow, TableCell, TableBody, ValueFormatter
 } from "@tremor/react";
 
 import { FactsAppEngagement as DataPkg } from "snowflake-demo-package-fast";
 
-
 const queries = (pkg: any) => {
 
-  const { gender, age, ethnicity, devicemakemodel, appTitle, foregroundduration, panelistid } = pkg.fields;
+  const { gender, age, ethnicity, devicemakemodel, appTitle, foregroundduration, panelistid, starttimestamp, isp } = pkg.fields;
 
   return {
-    genderSplit: pkg.select(
-      gender,
-      gender.count().as('genderCount'))
-      .filter(gender.isNotNull()),
-    ageSplit: pkg.select(
-      age,
-      age.count().as('ageCount'))
-      .filter(age.isNotNull()),
-    ethnicitySplit: pkg.select(
-      ethnicity,
-      ethnicity.count().as('ethnicityCount')),
+    sessionDuration: pkg.select(
+      starttimestamp.day.as('dayOfWeek'),
+      foregroundduration.avg().as('avgSessionLength'))
+      .filter(appTitle.eq('ESPN')),
+    appSplit: pkg.select(
+      appTitle,
+      panelistid.count().as('appCount'))
+      .filter(appTitle.isNotNull()),
+    deviceSplit: pkg.select(
+      devicemakemodel,
+      panelistid.count().as('deviceCount'))
+      .filter(devicemakemodel.isNotNull()),
+    ispSplit: pkg.select(
+      isp,
+      panelistid.count().as('ispCount'))
+      .filter(isp.isNotNull()),
     topSessions: pkg.select(
+      panelistid,
       devicemakemodel,
       appTitle,
       foregroundduration)
+    .filter(devicemakemodel.isNotNull().and(appTitle.isNotNull()))
     .orderBy([foregroundduration, 'DESC']),
     popSummary: pkg.select(
-      ethnicity,
-      age,
-      gender,
-      panelistid
-    ).limit(50)
+      appTitle,
+      devicemakemodel,
+      isp,
+      panelistid)
+    .limit(50)
   };
 };
 
 export default async function Home() {
 
-  const activePkg = DataPkg;
+  const activePkg = DataPkg; 
 
   const [
-    genderData,
-    ageData,
-    ethnicityData,
+    appData,
+    deviceData,
+    ispData,
     sessionData,
-    popData
+    popData,
+    durationData
   ] = await (Promise.all([
-    queries(activePkg).genderSplit.execute(),
-    queries(activePkg).ageSplit.execute(),
-    queries(activePkg).ethnicitySplit.execute(),
+    queries(activePkg).appSplit.limit(10).orderBy(["appCount", "desc"]).execute(),
+    queries(activePkg).deviceSplit.limit(10).orderBy(["deviceCount", "desc"]).execute(),
+    queries(activePkg).ispSplit.limit(10).orderBy(["ispCount","desc"]).execute(),
     queries(activePkg).topSessions.limit(50).execute(),
-    queries(activePkg).popSummary.execute()
+    queries(activePkg).popSummary.execute(),
+    queries(activePkg).sessionDuration.limit(7).execute().then((data) => {
+      // let newData = new Object
+      data.forEach(element => {
+        element.avgSessionLength = parseInt(element.avgSessionLength) / 1000;
+        element.dayOfWeek = element.dayOfWeek - 1;    
+      });
+      console.log(data);
+      return data; 
+    })
   ]));
 
   return (
     <main className="p-12">
-      <Title>County Demographic Summary</Title>
-      <Text>High Level Demographic Summary for All Counties</Text>
+      <Title>Mobile App Engagement Summary</Title>
+      <Text>Understanding how people use various mobile apps</Text>
 
       <TabGroup className="mt-6">
         <TabList>
@@ -66,38 +82,51 @@ export default async function Home() {
         </TabList>
 
         <TabPanels>
+          <Card>
+            <Title>Session Duration by Day of Week (seconds)</Title>
+            <BarChart 
+              className="mt-6"
+              data={durationData}
+              index="dayOfWeek"
+              categories={["avgSessionLength"]}
+              colors={["blue"]}
+              showLegend={true}
+              />
+          </Card>
+          
 
           <Grid numItems={3} className="mt-6 gap-6">
             <Card>
-              <Title>Gender Breakdown</Title>
+              <Title>App Breakdown</Title>
               <DonutChart
                 className="mt-6"
-                data={genderData}
-                category="genderCount"
-                index="gender"
-                colors={["rose", "violet"]}
+                data={appData}
+                category="appCount"
+                index="appTitle"
+                // colors={["rose", "violet"]}
+
               />
             </Card>
 
             <Card>
-              <Title>Age Breakdown</Title>
+              <Title>Device Breakdown</Title>
               <DonutChart
                 className="mt-6"
-                data={ageData}
-                category="ageCount"
-                index="age"
-                colors={["rose", "violet", "slate", "indigo", "cyan", "amber"]}
+                data={deviceData}
+                category="deviceCount"
+                index="devicemakemodel"
+                // colors={["rose", "violet", "slate", "indigo", "cyan", "amber"]}
               />
             </Card>
 
             <Card>
-              <Title>Ethnicity Breakdown</Title>
+              <Title>ISP Breakdown</Title>
               <DonutChart
                 className="mt-6"
-                data={ethnicityData}
-                category="ethnicityCount"
-                index="ethnicity"
-                colors={["rose", "violet", "slate", "indigo", "cyan", "amber"]}
+                data={ispData}
+                category="ispCount" 
+                index="isp"
+                colors={["rose", "violet", "slate", "indigo", "cyan", "amber", "blue", "green"]}
               />
             </Card>
 
@@ -114,14 +143,14 @@ export default async function Home() {
                   </TableHead>
 
                   <TableBody>
-                    {popData.map((row: any) => (
+                    {sessionData.map((row: any) => (
                       <TableRow key={row.panelistid}>
-                        <TableCell>{row.panelistid}</TableCell>
+                        <TableCell>{row.foregroundduration}</TableCell>
                         <TableCell>
-                          <Text>{row.ethnicity}</Text>
+                          <Text>{row.appTitle}</Text>
                         </TableCell>
                         <TableCell>
-                          <Text>{row.age}</Text>
+                          <Text>{row.devicemakemodel}</Text>
                         </TableCell>
                       </TableRow>
                     ))}
